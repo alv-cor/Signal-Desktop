4d188ad317f1c55fa001dd078c42396d70c33ff5
alv-cor <58772226+alv-cor@users.noreply.github.com>
2025-06-08 19:48:25 +0200

    Update get-expirte-time.ts

--- ts/scripts/get-expire-time.ts
+++ ts/scripts/get-expire-time.ts
@@ -2,18 +2,13 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 
 import { join } from 'path';
-import { execSync } from 'child_process';
 import { writeFileSync } from 'fs';
 
 import { DAY } from '../util/durations';
 import { version } from '../../package.json';
 import { isNotUpdatable } from '../util/version';
 
-const unixTimestamp = parseInt(
-  process.env.SOURCE_DATE_EPOCH ||
-    execSync('git show -s --format=%ct').toString('utf8'),
-  10
-);
+const unixTimestamp = Date.now();
 const buildCreation = unixTimestamp * 1000;
 
 // NB: Build expirations are also determined via users' auto-update settings; see
